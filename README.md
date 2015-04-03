dabfcdccbec30af75cf57bb4e464e4aab39194c3
sabik <sabik@mandroslabs.com>
2015-04-03 03:08:03 -0600

    initial, readme and ported tests

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# yaws
-Yet Another Web Scraper
+yawscraper
+===========
+
+[![Build Status](https://travis-ci.org/ivansabik/yaws.svg)](https://travis-ci.org/ivansabik/yaws)
+
+yawscraper (Yet Another Web Scraper) is a simple to use yet very powerful web scraping util for NodeJS.
+Uses following libs:
+
+- express
+- cheerio
+- mocha
+- assert-diff
+- nock
+- request-json
+
+## Install
+
+`npm install yaws`
+
+## Run tests
+
+`mocha`
+
+## Usage
+
+yaws.scrape(options, callback)
+
+#### Options
+
+url: The url to scrape
+html: Optionally scrape an HTML
+ua: Header for User-agent to use, default is 'chrome'. Other available ua's are chrome, firefox, explorer, safari, android, iphone, blackberry
+referer: Header for Referer to use
+cookie: Header for Cookies
+searchElement = The element looked for (td, span, div, etc.), default is 'text'
+options.allOcurrencies = When false, gets single result not all of them, default is true
+  
+#### Callback
+
+Function that is called when scraping is done
+
+## Examples
+
+var yaws = require('jaws')
+
+#### IMDB
+
+```
+var options = {}
+options.pattern = {
+}
+options.url = 'http://www.imdb.com/movies-in-theaters/'
+yaws.scrape(options, function(response) {
+  console.log(response);
+});
+```
+
+#### Mobile NYTimes
+
+```
+var options = {}
+options.pattern = {
+}
+options.url = 'http://mobile.nytimes.com/international/'
+options.ua = 'iphone'
+yaws.scrape(options, function(response) {
+  console.log(response);
+});
+```
+
+## Scrape patterns
+
+Patterns are regular expressions or elements (ids, classes, etc)
---
+++ test/yaws.js
@@ -0,0 +1,82 @@
+var assert = require('assert-diff')
+var fs = require('fs');
+var scraper = require('../lib/yaws.js');
+var nock = require('nock');
+
+describe('Scraper', function(){
+  it('should create scrapped objects with info based on a search pattern', function(done) {
+    nock('')
+      .get('')
+      .reply(200, html);
+    var testPattern = {
+    };
+    var testOptions = {
+      pattern: testPattern,
+      html: '',
+    }
+    scraper.scrape(testOptions, function(response) {
+      assert.deepEqual({}, response);
+      done();
+    });
+  });
+
+  it('should throw error when callback is not a function', function() {
+    var testFunction = function(){scraper.scrape({}, callback='console')};
+    var testFunction = function(){
+      scraper.scrape(testOptions);
+      done();
+    };
+    assert.throws(testFunction, Error, 'Callback is not a function!');
+  });
+
+  it('should remove whitespaces', function(done) {
+    nock('')
+      .get('')
+      .reply(200, html);
+    var testPattern = {
+    };
+    var testOptions = {
+      pattern: testPattern,
+      html: ''
+    }
+    scraper.scrape(testOptions, function(response) {
+      assert.deepEqual({}, response.pop());
+      done();
+    });
+  });
+
+  it('should throw error if pattern is not valid regexp', function(done) {
+    nock('')
+      .get('')
+      .reply(200, html);
+    var testPattern = {
+      testNotValidRegex: 'NOT VALID'
+    };
+    var testOptions = {
+      pattern: testPattern
+    }
+    var testFunction = function(){
+      scraper.scrape(testOptions);
+      done();
+    };
+    assert.throws(testFunction, Error, 'Pattern contains invalid regexp!');
+  });
+
+  it('should scrape from html string without making http requests', function(done) {
+    var mockHttp = nock('')
+      .get('')
+      .reply(200, html);
+    var testPattern = {
+      situacion: /.*agada/i
+    };
+    var testOptions = {
+      pattern: testPattern,
+      url: '',
+      html: html
+    }
+    scraper.scrape(testOptions, function(response) {
+      assert.equal(false, mockHttp.isDone());
+      done();
+    });
+  });
+});
